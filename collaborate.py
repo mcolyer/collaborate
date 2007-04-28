c14ddb3f0e3840d6a59cc2059a98087c524d95f7
tailor <matt@colyer.name>
2007-04-28 21:10:06 +0000

    [project @ Added Window helper to manage UI components]

--- collaborate.py
+++ collaborate.py
@@ -25,7 +25,8 @@ try:
         
         def activate(self, window):
             window.connect("tab-added", self.add)
-        
+            CollaborateWindowHelper(self, window)
+
         def deactivate(self, window):
             pass
         
@@ -89,7 +90,6 @@ try:
         def on_server_unfocus(self, widget, event, data=None):
             server = self.glade_tree.get_widget('server').get_text()
             orig_server = gconf.client_get_default().get_string(GCONF_KEY_JABBER_SERVER)
-            print "Unfocused"
             if orig_server != server:
                 self.retrieve_keyring_data(server)
 
@@ -111,7 +111,72 @@ try:
         def add(self, window, tab):
             print "opened"
             d = Document(tab.get_document())
-        
+    
+    # Menu item example, insert a new item in the Tools menu
+    ui_str = """<ui>
+      <menubar name="MenuBar">
+        <menu name="ToolsMenu" action="Tools">
+          <placeholder name="ToolsOps_2">
+            <menuitem name="ExamplePy" action="ExamplePy"/>
+          </placeholder>
+        </menu>
+      </menubar>
+    </ui>
+    """
+    class CollaborateWindowHelper:
+        def __init__(self, plugin, window):
+            self._window = window
+            self._plugin = plugin
+
+            # Insert menu items
+            self._insert_menu()
+
+        def deactivate(self):
+            # Remove any installed menu items
+            self._remove_menu()
+
+            self._window = None
+            self._plugin = None
+            self._action_group = None
+
+        def _insert_menu(self):
+            # Get the GtkUIManager
+            manager = self._window.get_ui_manager()
+
+            # Create a new action group
+            self._action_group = gtk.ActionGroup("CollaboratePluginActions")
+            self._action_group.add_actions([("ExamplePy", None, "Share document",
+                                             None, "Share the document",
+                                             self.on_share_document_activate)])
+
+            # Insert the action group
+            manager.insert_action_group(self._action_group, -1)
+
+            # Merge the UI
+            self._ui_id = manager.add_ui_from_string(ui_str)
+
+        def _remove_menu(self):
+            # Get the GtkUIManager
+            manager = self._window.get_ui_manager()
+
+            # Remove the ui
+            manager.remove_ui(self._ui_id)
+
+            # Remove the action group
+            manager.remove_action_group(self._action_group)
+
+            # Make sure the manager updates
+            manager.ensure_update()
+
+        def update_ui(self):
+            self._action_group.set_sensitive(self._window.get_active_document() != None)
+
+        # Menu activate handlers
+        def on_share_document_activate(self, action):
+            doc = self._window.get_active_document()
+            if not doc:
+                return
+
     class Document:
         socket = None
         _geditDocument = None
