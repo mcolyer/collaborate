92997cfae27abe9360a958d82d9c71e376b9bb5d
tailor <matt@colyer.name>
2007-06-01 19:47:16 +0000

    [project @ Alpha]
    This was alot of work but now there is a join and quit feature as well as remote highlighting. I am sure remote exploit possibilities exist and the code is a mess but now it can be dogfooded.

--- collaborate.py
+++ collaborate.py
@@ -10,7 +10,6 @@ import sys
 import gtk
 import gobject
 import gconf
-import gtk.glade
 import re
 import os.path
 import time
@@ -20,8 +19,11 @@ import md5
 import dbus
 import telepathy
 
+# Connect dbus to the glib mainloop
+from dbus.mainloop.glib import DBusGMainLoop
+DBusGMainLoop(set_as_default=True)
+
 GCONF_KEY_BASE = '/apps/gedit-2/plugins/collaborate'
-GLADE_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), 'collaborate.glade'))
 MISSION_CONTROL_BUS = 'org.freedesktop.Telepathy.Houston.MissionControl'
 MISSION_CONTROL_BUS_PATH = '/org/freedesktop/Telepathy/Houston/MissionControl'
 
@@ -71,9 +73,13 @@ try:
 
             # Create a new action group
             self._action_group = gtk.ActionGroup('CollaboratePluginActions')
-            self._action_group.add_actions([('Collaborate', None, 'Share document',
-                                             None, 'Share the document',
+            self._action_group.add_actions([('Share', None, 'Share document',
+                                             None, 'Share the current document',
                                              self.on_share_document_activate)])
+            self._action_group.add_actions([('Join', None, 'Join a document',
+                                             None, 'Join a shared document',
+                                             self.on_join_document_activate)])
+
 
             manager.insert_action_group(self._action_group, -1)
             
@@ -82,7 +88,9 @@ try:
                   <menubar name="MenuBar">
                     <menu name="ToolsMenu" action="Tools">
                       <placeholder name="ToolsOps_2">
-                        <menuitem name="Collaborate" action="Collaborate"/>
+                        <menuitem name="CollaborateShare" action="Share"/>
+                        <menuitem name="CollaborateJoin" action="Join"/>
+                        <separator/>
                       </placeholder>
                     </menu>
                   </menubar>
@@ -104,16 +112,44 @@ try:
 
         def on_share_document_activate(self, action):
             doc = self._window.get_active_document()
+            view = self._window.get_active_view()
             if not doc:
                 return
-            self._documents.append(Document(self._plugin, doc))
+            self._documents.append(Document(self._plugin, doc, view))
 
+        def on_join_document_activate(self, action):
+            dialog = gtk.Dialog(title='Document Address', 
+                                parent=self._window,
+                                buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT),
+                                flags=gtk.DIALOG_MODAL,
+                                )
+            entry = gtk.Entry()
+            dialog.vbox.pack_start(entry, padding=6)
+            dialog.connect('response', self.document_address_response, entry)
+            dialog.show_all()
+
+        def document_address_response(self, dialog, response_id, data=None):
+            #FIXME: Validate address input
+            document_address = data.get_text()
+            dialog.destroy()
+
+            if response_id == gtk.RESPONSE_REJECT:
+                return
+
+            self._window.create_tab(True)
+            doc = self._window.get_active_document()
+            view = self._window.get_active_view()
+            if not doc:
+                return
+            self._documents.append(Document(self._plugin, doc, view, document_address))
+            
 
     class Document:
-        _geditDocument = None
+        _gedit_document = None
 
-        def __init__(self, plugin, doc):
+        def __init__(self, plugin, doc, view, remote_channel_address=None):
             self._plugin = plugin
+            self._participants = {}
 
             # Ask Houston if there are any jabber accounts available.
             bus = dbus.SessionBus()
@@ -123,32 +159,78 @@ try:
             
             if len(jabber_connected_accounts) == 0:
                 # TODO: If none then alert the user to add an account.
-                pass
+                window = gtk.MessageDialog(parent=None,
+                                           type=gtk.MESSAGE_INFO,
+                                           buttons=gtk.BUTTONS_CLOSE,
+                                           flags=gtk.DIALOG_MODAL,
+                                           message_format="You must have a connected jabber account before you are able to share a document. Please make sure that an account is available and connected.")
+                window.connect('response', self.account_message_response)
+                window.show()
+
             elif len(jabber_connected_accounts) == 1:
                 # If 1 then use it
                 proxy_object = bus.get_object(jabber_connected_accounts[0][0], jabber_connected_accounts[0][1])
                 connection = dbus.Interface(proxy_object, telepathy.interfaces.CONN_INTERFACE)
-                handle = connection.RequestHandles(telepathy.CONNECTION_HANDLE_TYPE_ROOM, ['collaborate-test@conference.jabber.org'])[0]
+                handle = connection.RequestHandles(telepathy.CONNECTION_HANDLE_TYPE_ROOM, [remote_channel_address])[0]
                 transport_path = connection.RequestChannel(telepathy.interfaces.CHANNEL_TYPE_TEXT, telepathy.CONNECTION_HANDLE_TYPE_ROOM, handle, True)
                 proxy_object = bus.get_object(jabber_connected_accounts[0][0], transport_path)
                 self._transport = dbus.Interface(proxy_object, telepathy.interfaces.CHANNEL_TYPE_TEXT)
-                self._transport.Send(telepathy.CHANNEL_TEXT_MESSAGE_TYPE_NORMAL, 'Hello World')
             else:
                 # TODO: If >1 then allow them to choose one
                 pass
 
-            self._geditDocument = doc
-            self._geditDocument.connect('insert-text', self.insert)
-            self._geditDocument.connect('delete-range', self.delete)
+            # Connect up the recieved messages to be parsed.
+            self._transport.connect_to_signal("Received", self.handle_message)
+
+            self._gedit_document = doc
+            self._gedit_view = view
+            self._insert_signal_id = self._gedit_document.connect('insert-text', self.insert)
+            self._delete_signal_id = self._gedit_document.connect('delete-range', self.delete)
         
+        def account_message_response(self, dialog, response_id, data=None):
+            dialog.destroy()
+            bus = dbus.SessionBus()
+            mission_control = bus.get_object(MISSION_CONTROL_BUS, MISSION_CONTROL_BUS_PATH)
+            mission_control.ShowAccounts()
+
         def deactivate(self):
             pass
 
-        def execute(self, command):
+        def handle_message(self, id, timestamp, sender, type, flags, text):
+            try:
+                # Attempt to parse the message
+                operation = Operation.parse(str(text))
+
+                # Lock the document from editing and remove callbacks
+                self._gedit_view.set_property('editable', False)
+                self._gedit_document.disconnect(self._insert_signal_id)
+                self._gedit_document.disconnect(self._delete_signal_id)
+
+                # Perform the operation
+                self.execute(operation, sender)
+
+                # Add the callbacks and allow editing again
+                self._insert_signal_id = self._gedit_document.connect('insert-text', self.insert)
+                self._delete_signal_id = self._gedit_document.connect('delete-range', self.delete)
+                self._gedit_view.set_property('editable', True)
+
+            except InvalidOperation:
+                # Ignore invalid messages for now
+                pass
+
+        def execute(self, command, participant):
+            # FIXME: Should really use the tag table instead of my own hash table
+            if not self._participants.has_key(participant):
+                self._participants[participant] = self._gedit_document.create_tag()
+                
+                #FIXME: Get more creative here, limited to only 6 colors
+                self._participants[participant].set_property('background', 
+                        ['#FFF595', '#ADC7E3', '#C9F3A0', '#FFD394', '#D4BCD2','#F89999'][len(self._participants) % 7])
+
             if isinstance(command, InsertOperation):
-                self._geditDocument.insert(self._geditDocument.get_iter_at_offset(command.p), command.s)
+                self._gedit_document.insert_with_tags(self._gedit_document.get_iter_at_offset(command.p), command.s, self._participants[participant])
             elif isinstance(command, DeleteOperation):
-                self._geditDocument.delete(self._geditDocument.get_iter_at_offset(command.p), self._geditDocument.get_iter_at_offset(command.p+command.l))
+                self._gedit_document.delete(self._gedit_document.get_iter_at_offset(command.p), self._gedit_document.get_iter_at_offset(command.p+command.l))
 
         def insert(self, textbuffer, iter, text, length, data=None):
             self._transport.Send(telepathy.CHANNEL_TEXT_MESSAGE_TYPE_NORMAL, str(InsertOperation(text, iter.get_offset()))+"\n")
@@ -159,10 +241,16 @@ try:
             self._transport.Send(telepathy.CHANNEL_TEXT_MESSAGE_TYPE_NORMAL, str(DeleteOperation(length, offset)))
 
         def hash(self):
-            return md5.new(this._geditDocument.get_uri()).digest()
+            return md5.new(this._gedit_document.get_uri()).digest()
     
 except:
     print "WARNING: No Gedit environment"
+
+class InvalidOperation(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 class Operation:
     @staticmethod
@@ -178,6 +266,8 @@ class Operation:
             length = int(length)
             position = int(position)
             return DeleteOperation(length, position)
+        else:
+            raise InvalidOperation, "Invalid message string"
 
     def is_insert(self):
         return False
